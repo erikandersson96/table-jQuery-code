75f65c3d1df6b5a1b0acd8e1725cef2bc26a4ba2
erikandersson96 <erik96.andersson@gmail.com>
2022-05-17 15:55:51 +0000

    Add jQuery code from CI video

---
+++ js/my_script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("h2").css("text-decoration", "underline"); // underlines all <h2> elements
+    $("ul").css("border", "solid 1px grey"); //adds a border to all lists
+});
