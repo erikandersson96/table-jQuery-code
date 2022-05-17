e77ac5259d79a2c70b7b6ceeff38399798aea779
erikandersson96 <erik96.andersson@gmail.com>
2022-05-17 16:12:10 +0000

    Change th from spinach to broccoli

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,3 @@
 $(document).ready(function() {
-    $("h2").css("text-decoration", "underline"); // underlines all <h2> elements
-    $("ul").css("border", "solid 1px grey"); //adds a border to all lists
+    
 });
